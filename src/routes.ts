040462116939a56cbe4920f416e9cf173a7bfa4d
Tran Hoang Huy <huybeos2707@gmail.com>
2022-01-13 11:25:52 +0700

    update

--- src/routes.ts
+++ src/routes.ts
@@ -3,6 +3,8 @@ import { createUserHandler } from './controllers/user.controller';
 import { createUserSessionHandler, invalidateUserSessionHandler, getUserSessionsHandler } from './controllers/session.controller';
 import { validateRequest, requiresUser } from './middlewares';
 import { createUserSchema, createUserSessionSchema } from './schema/user.schema';
+import { createPostSchema, deletePostSchema, updatePostSchema } from './schema/post.schema';
+import { createPostHandler, deletePostHandler, getPostHandler, updatePostHandler } from './controllers/post.controller';
 
 export default function (app: Express) {
     app.get("/test", (req: Request, res: Response) => res.sendStatus(200));
@@ -22,4 +24,16 @@ export default function (app: Express) {
     //Logout
     //Delete /api/sessions
     app.delete("/api/sessions", requiresUser, invalidateUserSessionHandler)
+
+    //create a post
+    app.post("/api/posts", [requiresUser, validateRequest(createPostSchema)], createPostHandler);
+
+    // update a post
+    app.put("/api/posts/:postId", [requiresUser, validateRequest(updatePostSchema)], updatePostHandler);
+
+    // get a post
+    app.get("/api/posts/:postId", getPostHandler);
+
+    //delete a post
+    app.delete("/api/posts/:postId", [requiresUser, validateRequest(deletePostSchema)], deletePostHandler);
 }
